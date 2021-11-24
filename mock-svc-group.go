d5f19f073476d52fcdc72bc52643ded314006da6
slavayssiere <sebastien.lavayssiere@spoon.ai>
2021-11-24 14:17:10 +0100

    remove import groups in..;groups

--- mock-svc-group.go
+++ mock-svc-group.go
@@ -8,15 +8,13 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	groups_grpc "github.com/slavayssiere-spoon/groups"
 )
 
 type MockGrpcGroup struct {
 	cc *grpc.ClientConn
 }
 
-func (s MockGrpcGroup) Get(ctx context.Context, in *groups_grpc.Group, opts ...grpc.CallOption) (*groups_grpc.Group, error) {
+func (s MockGrpcGroup) Get(ctx context.Context, in *Group, opts ...grpc.CallOption) (*Group, error) {
 	if in.Id == 401 {
 		return nil, status.Error(codes.PermissionDenied, "permission denied")
 	} else if in.Id == 404 {
@@ -26,33 +24,33 @@ func (s MockGrpcGroup) Get(ctx context.Context, in *groups_grpc.Group, opts ...g
 	} else if in.Id == 501 {
 		return nil, status.Error(codes.Unavailable, "Unavailable")
 	}
-	return &groups_grpc.Group{
+	return &Group{
 		Id:    in.Id,
 		Name:  "group-" + fmt.Sprint(in.Id),
 		Paths: []string{"group-" + fmt.Sprint(in.Id)},
 	}, nil
 }
 
-func (s MockGrpcGroup) GetAll(ctx context.Context, in *groups_grpc.Groups, opts ...grpc.CallOption) (*groups_grpc.Groups, error) {
+func (s MockGrpcGroup) GetAll(ctx context.Context, in *Groups, opts ...grpc.CallOption) (*Groups, error) {
 	return nil, nil
 }
 
-func (s MockGrpcGroup) GetByName(ctx context.Context, in *groups_grpc.Group, opts ...grpc.CallOption) (*groups_grpc.Group, error) {
+func (s MockGrpcGroup) GetByName(ctx context.Context, in *Group, opts ...grpc.CallOption) (*Group, error) {
 	return nil, nil
 }
 
-func (s MockGrpcGroup) GetGraph(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*groups_grpc.Groups, error) {
+func (s MockGrpcGroup) GetGraph(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*Groups, error) {
 	return nil, nil
 }
 
-func (s MockGrpcGroup) Add(ctx context.Context, in *groups_grpc.Group, opts ...grpc.CallOption) (*groups_grpc.Group, error) {
+func (s MockGrpcGroup) Add(ctx context.Context, in *Group, opts ...grpc.CallOption) (*Group, error) {
 	return nil, nil
 }
 
-func (s MockGrpcGroup) Update(ctx context.Context, in *groups_grpc.Group, opts ...grpc.CallOption) (*groups_grpc.Group, error) {
+func (s MockGrpcGroup) Update(ctx context.Context, in *Group, opts ...grpc.CallOption) (*Group, error) {
 	return nil, nil
 }
 
-func (s MockGrpcGroup) AddSubGroup(ctx context.Context, in *groups_grpc.Group, opts ...grpc.CallOption) (*groups_grpc.Group, error) {
+func (s MockGrpcGroup) AddSubGroup(ctx context.Context, in *Group, opts ...grpc.CallOption) (*Group, error) {
 	return nil, nil
 }
