83e0cbe7627dc049ecbc60a2c9d681d145c2adc2
slavayssiere-spoon <94708047+slavayssiere-spoon@users.noreply.github.com>
2021-12-10 15:03:23 +0100

    Update svcGroups.py

--- groups/svcGroups.py
+++ groups/svcGroups.py
@@ -23,8 +23,8 @@ class SvcGroups():
     self.stub = groupsStub(channel)
     print("connected :-)")
 
-  def GetPaths(self, identity):
-    metadata = (('authorization', g.bearer),
+  def GetPaths(self, bearer, identity):
+    metadata = (('authorization', bearer),
             ('some-md-key', 'another value'))
     grp = self.stub.Get(request=Group(name=identity), metadata=metadata)
     print(grp.id)
